637e847c79d8a2c4427f3d718ed62b46f20145e5
Sacha Trémoureux <sacha@tremoureux.fr>
2017-08-06 18:54:14 +0200

    Basic web interface

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	//	"github.com/skratchdot/open-golang/open"
+
 	"encoding/json"
-	"framagit.org/tsacha-xmpp/xmpp"
+	libxmpp "framagit.org/tsacha-xmpp/xmpp"
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+
 	"io/ioutil"
+	"net/http"
 	"os/user"
+
+	"fmt"
 )
 
 type Configuration struct {
@@ -12,8 +21,18 @@ type Configuration struct {
 	Password string
 }
 
-func main() {
+type Connection struct {
+	Jid      string `json:"jid"`
+	Password string `json:"password"`
+	Resource string `json:"resource"`
+}
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func GetCredentials() (string, string) {
 	usr, _ := user.Current()
 
 	file, _ := ioutil.ReadFile(
@@ -22,7 +41,67 @@ func main() {
 	var c Configuration
 	json.Unmarshal(file, &c)
 
-	xmpp.Connect(c.Account, c.Password)
+	return c.Account, c.Password
+}
+
+func testRoster(account string, password string, resource string) []byte {
+
+	xmpp := libxmpp.Connect(account, password, resource)
+
+	go xmpp.InfinitePing()
+
+	xmpp.Disco("tremoureux.fr")
+	xmpp.Disco(account)
+
+	xmpp.GetRoster()
+	contacts, _ := json.Marshal(xmpp.State.Roster.Contacts)
+
+	xmpp.Close()
+
+	return contacts
+}
+
+func WsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Client subscribed")
+	for {
+		mt, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		data := Connection{}
+		json.Unmarshal(msg, &data)
+
+		fmt.Println(data.Jid)
+
+		conn.WriteMessage(mt, testRoster(data.Jid, data.Password, data.Resource))
+	}
+}
+
+func main() {
+	// mux handler
+	router := mux.NewRouter()
+
+	router.HandleFunc("/websocket", WsHandler)
+
+	// Serve static assets via the "static" directory
+	fs := rice.MustFindBox("static").HTTPBox()
+
+	staticFileServer := http.StripPrefix("/", http.FileServer(fs))
+	router.Handle("/{path:.*}", staticFileServer)
+
+	// Open web browser
+	//	go open.Start("http://localhost:5282")
+
+	// Serve this program forever
+	go http.ListenAndServe(":5282", router)
 
 	select {}
 }
